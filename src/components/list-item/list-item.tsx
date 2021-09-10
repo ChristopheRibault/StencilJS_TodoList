19a48cc423567dac7eb40b6e978c1daef7174390
ChristopheRibault <c.ribault.dev@gmail.com>
2021-09-10 16:21:39 +0200

    feat: edit todo

--- src/components/list-item/list-item.tsx
+++ src/components/list-item/list-item.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop } from '@stencil/core';
+import { Component, h, Prop, State } from '@stencil/core';
 
 @Component({
   tag: 'list-item',
@@ -6,19 +6,29 @@ import { Component, h, Prop } from '@stencil/core';
   shadow: true,
 })
 export class ListItem {
+  @State() editionMode: boolean;
   @Prop() text: string;
   @Prop() deleteItem;
 
   delete() {
     this.deleteItem(this.text);
   }
+  toggleEdit() {
+    this.editionMode = !this.editionMode
+  }
+  handleChange(e) {
+    this.text = e.target.value
+  }
 
   render() {
     return (
       <li class='list-item'>
         <input type="checkbox" name="" id="" />
-        <p>{this.text}</p>
-        <button>editer</button>
+        {this.editionMode ?
+          <input id='item-input' class='hidden' type="text" value={this.text} onInput={(e) => this.handleChange(e) } /> :
+          <p id='item-text'>{this.text}</p>
+        }
+        <button onClick={() => this.toggleEdit()}>editer</button>
         <button onClick={() => this.delete()}>supprimer</button>
       </li>
     );
